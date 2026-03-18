e88c68cb9dac7037a8ebbd5bdcf89d8732586407
Ashwani Mittal <mittal_ashwani@yahoo.com>
2026-03-18 15:17:03 +0530

    Initialize SQLite database with new device metadata table
    - Create device_info table for device metadata and firmware tracking
    - Insert initial device metadata for PUMP_01, PUMP_02, and TURBINE_A devices
    - Use INSERT OR IGNORE to handle duplicate device entries safely

--- analysis_queries.sql
+++ analysis_queries.sql
@@ -1,5 +1,9 @@
+-- This SQL script contains various queries to analyze the telemetry data collected from devices.
+
+-- Analysis Queries for Telemetry Data
 SELECT * FROM telemetry;
 
+-- Calculate average temperature and voltage for each device
 SELECT 
     device_id, 
     MAX(temperature) AS max_temp, 
@@ -8,6 +12,33 @@ SELECT
 FROM telemetry
 GROUP BY device_id;
 
+-- Find all records where the error code is not zero and order by temperature
 SELECT * FROM telemetry 
 WHERE error_code != 0 
 ORDER BY temperature DESC;
+
+
+-- Join telemetry data with device information to get more context
+-- This query retrieves the timestamp, device ID, location, and temperature 
+-- for each telemetry record by joining the telemetry table with the device_info 
+-- table on the device_id field.
+SELECT 
+    telemetry.timestamp,
+    telemetry.device_id,
+    device_info.location,
+    telemetry.temperature
+FROM telemetry
+INNER JOIN device_info 
+    ON telemetry.device_id = device_info.device_id;
+
+-- Find all devices that have reported an error and their corresponding 
+-- firmware version is old which is 'v1.0.2'. This query joins the telemetry 
+-- table with the device_info
+SELECT 
+    t.device_id, 
+    t.error_code, 
+    i.firmware_rev,
+    i.location
+FROM telemetry AS t
+JOIN device_info AS i ON t.device_id = i.device_id
+WHERE t.error_code != 0 AND i.firmware_rev = 'v1.0.2';
--- db_setup.py
+++ db_setup.py
@@ -23,11 +23,35 @@ cursor.execute('''
 #     VALUES ('DEV_001', 24.5, 3.3, 0)
 # ''')
 
-conn.commit()
+# conn.commit()
 
 # cursor.execute("SELECT * FROM telemetry")
 # for row in cursor.fetchall():
 #     print(row)
     
+
+# Create a Metadata table
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS device_info (
+        device_id TEXT PRIMARY KEY,
+        location TEXT,
+        firmware_rev TEXT,
+        install_date DATE
+    )
+''')
+
+# Insert details for the devices we used in our generator
+devices_metadata = [
+    ('PUMP_01', 'Basement-Zone-A', 'v1.0.2', '2025-01-15'),
+    ('PUMP_02', 'Roof-Top-North', 'v1.0.4', '2025-02-10'),
+    ('TURBINE_A', 'Main-Floor', 'v2.1.0', '2024-11-20')
+]
+
+cursor.executemany('''INSERT OR IGNORE INTO device_info VALUES
+                   (?, ?, ?, ?)''', devices_metadata)
+
+
+
+conn.commit()
 conn.close()
-print("Database and table created successfully!")
+print("Database and tables are created successfully!")
